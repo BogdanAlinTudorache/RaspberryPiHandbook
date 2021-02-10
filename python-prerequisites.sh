07b88d333974bf2559d2425e539ca619cdd5684a
Bogdan Tudorache <tudorache.a.bogdan@gmail.com>
2021-02-10 21:35:45 +0100

    Update python-prerequisites.sh
    added selenium and BS

--- python-prerequisites.sh
+++ python-prerequisites.sh
@@ -3,4 +3,6 @@ sudo apt-get upgrade;
 sudo apt-get install python3-pip;
 alias python3=python;
 alias pip3=pip;
-sudo pip install pandas; # both pandas and numpy
+sudo pip3 install pandas; # both pandas and numpy
+sudo pip3 install selenium;
+sudo pip3 install beautifulsoup4;
