d4e529a34c8167b3ced4f21768c1a4512c8c3973
Bogdan Tudorache <tudorache.a.bogdan@gmail.com>
2023-10-07 22:13:20 +0200

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -4,7 +4,8 @@ import time
 from bs4 import BeautifulSoup as soup
 
 # Configuration
-headers = {'user-agent': 'Mozilla/5.0'}
+headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36' }
+
 url = "http://berrynews.org/netherlands-en.html?"
 sleep_time = 1  # time in seconds
 
